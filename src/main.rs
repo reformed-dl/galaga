80a1a1ff5691ce78e6e27019854f69eb1f633542
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-11 21:36:45 -0500

    Tasks 1 and 2 with Tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//Game Board Dimensions
+const BOARD_WIDTH: u32 = 20;
+const BOARD_HEIGHT: u32 = 20;
+
+//Player Value
+const STARTING_LIVES: u8 = 5;
+const DISPLAY_CHARACTER: char = '$';
+
+
 fn main() {
-    println!("Hello, Epic");
+    println!("Game Board Dimensions {} x {}", BOARD_WIDTH, BOARD_HEIGHT);
+    println!("Player Starting Lives: {}", STARTING_LIVES);
+    println!("Player Character: {}", DISPLAY_CHARACTER);
 }
