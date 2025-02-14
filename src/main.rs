ec49fe7939160740164df3084c389e879671aa27
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-14 06:46:31 -0500

    completing task 3

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,35 @@ const BOARD_HEIGHT: u32 = 20;
 const STARTING_LIVES: u8 = 5;
 const DISPLAY_CHARACTER: char = '$';
 
+//Player Struct
+#[derive(Debug)]
+
+pub struct Player {
+    display_character: char,
+    lives: u8,
+    current_position: (u8, u8),
+}
+
+//Player Implementation
+impl Player {
+    fn new(start_position: (u8, u8)) -> Self {
+        Player {
+            display_character: DISPLAY_CHARACTER,
+            lives: STARTING_LIVES,
+            current_position: start_position,
+        }
+    }
+}
+
 
 fn main() {
     println!("Game Board Dimensions {} x {}", BOARD_WIDTH, BOARD_HEIGHT);
     println!("Player Starting Lives: {}", STARTING_LIVES);
     println!("Player Character: {}", DISPLAY_CHARACTER);
+    let player = Player {
+        display_character: '$',
+        lives: STARTING_LIVES,
+        current_position: (5, 5),
+    };
+    println!("Player Infor: {:?}", player);
 }
