0b41346abb14f2287c37345136144a938ecde176
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-15 08:19:07 -0500

    task 4 and 5, errors on #[derive(Debug)]

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ const BOARD_HEIGHT: u32 = 20;
 const STARTING_LIVES: u8 = 5;
 const DISPLAY_CHARACTER: char = '$';
 
+//HashMap from Collections
+std::collections::HashMap::
+
 //Player Struct
 #[derive(Debug)]
-
 pub struct Player {
     display_character: char,
     lives: u8,
@@ -25,6 +27,12 @@ impl Player {
         }
     }
 }
+//GameState Struct
+#[derive(Debug)]
+pub struct GameState {
+    game_board: HashMap<(u8, u8), char>,
+    player: &Player,
+}
 
 
 fn main() {
@@ -36,5 +44,14 @@ fn main() {
         lives: STARTING_LIVES,
         current_position: (5, 5),
     };
-    println!("Player Infor: {:?}", player);
+    println!("Player Info: {:?}", player);
+
+    let player = Player::new((5,5));
+    println!("Player Created: {:?}", player);
+
+    let mut game_board = HashMap::new();
+    game_board.insert((5,5), "$");
+    let player = Player::new((5,5));
+    let game_state = GameState{game_board, player: &player};
+    println!("Game State: {:?}", game_state);
 }
