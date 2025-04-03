8fac8b3b754e39b825a6f464e5f8ca91cba4a1d1
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-04-03 12:31:47 -0400

    Galaga Changes to initial tasks

--- src/main.rs
+++ src/main.rs
@@ -3,53 +3,25 @@ const BOARD_WIDTH: u32 = 20;
 const BOARD_HEIGHT: u32 = 20;
 
 //Player Value
-const STARTING_LIVES: u8 = 5;
+const STARTING_LIVES: u8 = 5; 
 const DISPLAY_CHARACTER: char = '$';
-
-std::collections::HashMap::
 
 //Player Struct
 #[derive(Debug)]
-pub struct Player {
+struct Player {
     display_character: char,
     lives: u8,
     current_position: (u8, u8),
 }
 
-//Player Implementation
-impl Player {
-    fn new(start_position: (u8, u8)) -> Self {
-        Player {
-            display_character: DISPLAY_CHARACTER,
-            lives: STARTING_LIVES,
-            current_position: start_position,
-        }
-    }
-}
-//GameState Struct
-#[derive(Debug)]
-pub struct GameState {
-    game_board: HashMap<(u8, u8), char>,
-    player: &Player,
-}
-
 fn main() {
-    println!("Game Board Dimensions {} x {}", BOARD_WIDTH, BOARD_HEIGHT);
-    println!("Player Starting Lives: {}", STARTING_LIVES);
-    println!("Player Character: {}", DISPLAY_CHARACTER);
+    println!("Gameboard Dimension are: {} x {}", BOARD_HEIGHT, BOARD_WIDTH);
+    println!("Player Starting Lives: {}, Symbol: {}", STARTING_LIVES, DISPLAY_CHARACTER);
+
     let player = Player {
-        display_character: '$',
+        display_character: DISPLAY_CHARACTER,
         lives: STARTING_LIVES,
         current_position: (5, 5),
     };
-    println!("Player Info: {:?}", player);
-
-    let player = Player::new((5,5));
-    println!("Player Created: {:?}", player);
-
-    let mut game_board = HashMap::new();
-    game_board.insert((5,5), "$");
-    let player = Player::new((5,5));
-    let game_state = GameState{game_board, player: &player};
-    println!("Game State: {:?}", game_state);
+    println!("Player Stats: {:?}", player);
 }
