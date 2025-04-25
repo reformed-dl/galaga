4da93e14c34e6d89f27556fd10202ccd3d6eb089
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-04-25 12:31:34 -0400

    Tasks 12-14 Complete

--- src/structs.rs
+++ src/structs.rs
@@ -2,3 +2,21 @@
 pub const SIZE: usize = 10;
 pub const ROWS: usize = SIZE;
 pub const COLUMNS: usize = SIZE * 2;
+
+
+//Cords Struct - Tuple
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Cords (pub usize, pub usize);
+
+//Time Struct - Named Field
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Timer {
+    current_timer: u64,
+    interval: u64,
+}
+
+impl Timer {
+    fn new(interval: u64) -> Self {
+        Self { current_timer: 0, interval, }
+    }
+}
